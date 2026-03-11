f73828d2013744cb871bb7ff464dd8817da2b3ac
shraracindna <thenameisshravan55@gmail.com>
2026-03-11 10:50:04 +0530

    .

---
+++ src/main5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class main5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        String reversed = "";
+
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
+
+        if (input.equals(reversed)) {
+            System.out.println("The given string is a palindrome.");
+        } else {
+            System.out.println("The given string is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
